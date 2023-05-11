919a63ccdfba37df3620f38aeecf637c88172748
shy978 <e850120@naver.com>
2023-05-11 22:05:35 +0900

    230511

---
+++ Table/Table/AddViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddViewController.swift
+//  Table
+//
+//  Created by DSC07 on 2023/05/11.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var tfAddItem: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnAddItem(_ sender: UIButton) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Table/Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by DSC07 on 2023/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
